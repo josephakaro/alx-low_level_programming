b61cf99fbeccd6e3654712375f54e2a05b6f0aca
josephmarko <josephmmakaro@gmail.com>
2023-06-15 14:40:12 -0700

    SizeOf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/**
+ *  main - A program that he size of various data types
+ *  Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+ 	printf("%lu", sizeof(double));
+	return (0);
+}
