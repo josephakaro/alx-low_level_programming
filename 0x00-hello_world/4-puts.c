ed7532a23d403f212a4ff1c57ff540ecfa3b24b0
josephmarko <josephmmakaro@gmail.com>
2023-06-15 06:49:19 -0700

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ *This program prints a string using puts function.
+ */
+
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle\n";
+		puts(str);
+
+	return (0);
+}
