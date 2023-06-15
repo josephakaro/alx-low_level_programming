d4f698417e2f3a8d478f84660010858758574295
josephmarko <josephmmakaro@gmail.com>
2023-06-15 09:04:47 -0700

    hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,8 +10,7 @@
 
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle\n";
-		puts(str);
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
