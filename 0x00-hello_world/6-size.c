235f14d192a48072fbb81441bbb1f3efe6b1fc7a
josephmarko <josephmmakaro@gmail.com>
2023-06-15 15:45:38 -0700

    Two system, one file :)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
-#include <stdio>
+#include <stdio.h>
 /**
- *  main - A program that he size of various data types
+ *  main - A program that compile the into two computer sys.
  *  Return: always 0 (success)
  */
 int main(void)
 {
-	printf("%lu\n", sizeof(char));
-	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(float));
- 	printf("%lu", sizeof(double));
+	printf("Size of a char: %lu byte (s)\n", sizeof(char));
+	printf("Size of an int: %lu byte (s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte (s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte (s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte (s)\n", sizeof(float));
 	return (0);
 }
