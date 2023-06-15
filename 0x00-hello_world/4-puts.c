3b6495e6487b3c9add6fcfee7880243f3d6b4451
josephmarko <josephmmakaro@gmail.com>
2023-06-15 07:16:23 -0700

    Puts function is used

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *This program prints a string using puts function.
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
 /*Assigned variable of type string*/
 	char str[] = "\"Programming is like building a multilingual puzzle\n";
