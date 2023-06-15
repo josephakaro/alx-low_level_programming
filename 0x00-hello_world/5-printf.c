60c1ce24b6527cb0fcd44cc9cfced017636ab39d
josephmarko <josephmmakaro@gmail.com>
2023-06-15 11:30:30 -0700

    All done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Printing a string with printf function'
+ * Return: Always 0  (success)
+ */
+int main(void)
+{
+	printf("with proper gramer, but the outcome is a piece of art,\n");
+	return (0);
+}
