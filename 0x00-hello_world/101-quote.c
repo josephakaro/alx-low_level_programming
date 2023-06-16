b7940ebb891c987421bb5c31ae5c8703e9bd0425
josephmarko <josephmmakaro@gmail.com>
2023-06-15 18:06:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,12 @@
-{ #include <stdio.h>
+#include <stdio.h>
+/**
+ * main - A program prints a quote and runs without error.
+ *
+ * Return: 1 (success)
+ */
 
-int main(void) { quote(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 100);
-
-return (1);
-} }
+int main(void)
+{
+	putF(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
