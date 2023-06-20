e8289c05f60702ddcd08d2a8605509ae6aaec651
josephmarko <josephmmakaro@gmail.com>
2023-06-20 15:40:55 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,12 +12,16 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= 'a' && c <= 'z')
 	{
-		return (0);
+		return (1);
+	}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
