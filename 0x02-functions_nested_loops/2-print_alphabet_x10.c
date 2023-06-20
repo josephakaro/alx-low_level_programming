b353e8e5cab0131552684b821532aec5fcd80e44
josephmarko <josephmmakaro@gmail.com>
2023-06-20 13:06:41 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description 'X10 alphabet from a-z'
+ *
+ * Return: always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int l = 0;
+
+	while (l <= 10)
+	{
+		int n;
+
+		for (n = 97; n <= 122; n++)
+		{
+			_putchar(n);
+		}
+
+		l++;
+		_putchar('\n');
+	}
+}
