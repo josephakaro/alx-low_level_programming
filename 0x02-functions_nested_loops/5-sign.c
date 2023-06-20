766a54620c014bd8575b54b1de75fecb9f02332a
josephmarko <josephmmakaro@gmail.com>
2023-06-20 15:57:43 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Description: prints - and + when n less or greater 
+ *
+ * @n: parameter checks
+ *
+ *Return: Always 0 (success)
+ */
+
+int print_sign(int n)
+{
+	if(n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+	}
+
+	return (-1);
+}
