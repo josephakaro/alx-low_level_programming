e12b77b4d7acbd78798456e30fd81c8ba84b70a7
josephmarko <josephmmakaro@gmail.com>
2023-06-20 16:13:28 -0700

    Absolute of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @n: chacks for absolute value  of int
+ *
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
