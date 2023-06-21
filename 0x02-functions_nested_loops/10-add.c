469c9c12f2999e645275abad9503806f7d014d24
josephmarko <josephmmakaro@gmail.com>
2023-06-21 14:33:52 -0700

    sum of x and y

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - sums to integers then prints the results
+ *
+ * @x: first value
+ * @y: Second Value
+ *
+ * Return: sum of x and y
+ */
+
+int add(int x, y);
+{
+	return (x + y);
+}
