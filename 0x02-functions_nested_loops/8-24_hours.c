6e50406fd35b4d82ccaba6b6ec5d634d9be3a6bd
josephmarko <josephmmakaro@gmail.com>
2023-06-20 17:50:26 -0700

    every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+void jack_bauer(void)
+{
+	int i, t;
+
+	i = 0;
+
+	for (i = 0; i < 24; i++)
+	{
+		t = 0;
+		while (t < 60)
+		{
+			int k = i / 10;
+			int l = i % 10;
+			int o = t / 10;
+			int f = t % 10;
+
+			_putchar(k + '0');
+			_putchar(l+ '0');
+			_putchar(':');
+			_putchar(o + '0');
+			_putchar(f+ '0');
+			_putchar('\n');
+			t++;
+		}
+	}
+}
