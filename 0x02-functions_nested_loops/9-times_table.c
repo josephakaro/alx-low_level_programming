640277ff47e285a1732958a2284ded301dd5b0f2
josephmarko <josephmmakaro@gmail.com>
2023-06-21 14:21:00 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 time tables to the console
+ *
+ * Return: Always 0 (success)
+ */
+
+void times_table(void)
+{
+	int m, n, calc;
+
+	m = 0;
+
+	while (m <= 9)
+	{
+		_putchar(48);
+		for (n = 1; n <= 9; n++)
+		{
+			calc = m * n;
+
+			_putchar(44);
+			_putchar(32);
+
+			if (calc <= 9)
+			{
+				_putchar(32);
+				_putchar(calc + 48);
+			}
+			else
+			{
+				_putchar((calc / 10) + 48);
+				_putchar((calc % 10) + 48);
+			}
+		}
+		_putchar('\n');
+}
