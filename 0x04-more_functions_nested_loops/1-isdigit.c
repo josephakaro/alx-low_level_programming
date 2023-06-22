9d35cf479eb767b8f53ded17e487f199e2154e83
josephmarko <josephmmakaro@gmail.com>
2023-06-22 13:51:40 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -11,15 +11,5 @@
  */
 int _isdigit(int c)
 {
-	for (c = 0; c <= 9; c++)
-	{
-		if (c == 0 && c <= 9)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
-	}
+	return (c >= 0 && c <= 9);
 }
