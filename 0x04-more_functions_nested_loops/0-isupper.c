88118fd9ea14d111714703d40cacec4f4f1302c4
josephmarko <josephmmakaro@gmail.com>
2023-06-22 13:37:09 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -12,7 +12,7 @@
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
@@ -20,5 +20,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,15 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 extern int _putchar(char c);
-extern void print_alphabet(void);
-extern void print_alphabet_x10(void);
-extern int _islower(int c);
-extern int _isalpha(int c);
-extern int print_sign(int n);
-extern int _abs(int);
-extern int print_last_digit(int);
-extern void jack_bauer(void);
-extern void times_table(void);
-extern int add(int, int);
-extern void print_to_98(int n);
-extern void print_times_table(int n);
+extern int _isupper(int c);
+extern int _isdigit(int c);
+extern int mul(int a, int b);
+extern void print_numbers(void);
+extern void print_most_numbers(void);
+extern void more_numbers(void);
+extern void print_line(int n);
+extern void print_diagonal(int n);
+extern void print_square(int size);
+extern void print_triangle(int size);
+extern void print_number(int n);
 #endif
