b0f65b706162297f7bd192525b0f01d97c849c43
josephmarko <josephmmakaro@gmail.com>
2023-06-22 18:08:33 -0700

    hash lines

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int k, j;
+
+		k = 0;
+
+		while (k < size)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+				_putchar('\n');
+				k++;
+		}
+	}
+}
