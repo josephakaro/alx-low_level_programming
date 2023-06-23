1b81af9e4d3b43277df5c9a31883c69c5d8d02a5
josephmarko <josephmmakaro@gmail.com>
2023-06-22 17:55:03 -0700

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ *
+ * @n: number of times the character \ should be printed
+ *
+ * Return: Always 0 (success)
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int l, m;
+
+		l = 0;
+
+		while (l < n)
+		{
+			m = 0
+
+			while (m < n)
+			{
+				if (m == l)
+					_putchar('\\');
+				else if (m < l)
+					_putchar(' ');
+				m++;
+			}
+			_putchar('\n');
+			l++;
+		}
+	}
+}
