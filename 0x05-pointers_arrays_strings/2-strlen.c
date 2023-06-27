db389f0aaf32c14c0385d0e8186762b23e636c85
josephmarko <josephmmakaro@gmail.com>
2023-06-27 04:13:51 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function that reutrns the length of a string
+ *
+ *@s: string to validate
+ *
+ * Return: Length of a string
+ */
+
+int _strlen(char *s)
+{
+	int str;
+
+	for (str = 0; s[str] != '\0'; str++)
+
+	return (str);
+}
