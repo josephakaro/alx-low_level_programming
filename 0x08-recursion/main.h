605a91741bd70a331d2f23475853633890948f40
josephmarko <josephmmakaro@gmail.com>
2023-07-05 06:21:47 -0700

    Recrsive string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+extern int _putchar(char c);
+extern void _puts_recursion(char *s);
+extern void _print_rev_recursion(char *s);
+extern int _strlen_recursion(char *s);
+extern int factorial(int n);
+extern int _pow_recursion(int x, int y);
+extern int _sqrt_recursion(int n);
+extern int is_prime_number(int n);
+extern int is_palindrome(char *s);
+extern int wildcmp(char *s1, char *s2);
+
+#endif
