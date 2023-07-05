9b1cd259508851f2c08f11a99e341db874bced26
josephmarko <josephmmakaro@gmail.com>
2023-07-05 06:27:29 -0700

    Recursion string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,9 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	s = _puts_recursion(s);
-
-	_putchar('\n');
-
-	return (s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_ptuchar(*s);
+	_puts_recursion(s + 1);
 }
