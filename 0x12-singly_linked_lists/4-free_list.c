3d791d887237779426fe1ea17ab68777c2742f04
josephmarko <josephmmakaro@gmail.com>
2023-07-27 17:50:13 -0700

    temp add to rask 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *fo;
+	list_t *temp;
 
 	while (head)
 	{
-		fo = head->next;
+		temp = head->next;
 		free(head->str);
 		free(head);
-		head = fo;
+		head = temp;
 	}
 }
