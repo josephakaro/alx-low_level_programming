b3221f17b40a914695f76f0be0c035f9cc656359
josephmarko <josephmmakaro@gmail.com>
2023-07-29 09:34:04 -0700

    Betty Style Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,29 +10,28 @@
 
 int main(void)
 {
-    int i = '0';
-    int j;
+	int i = '0';
+	int j;
 
-    while (i <= '9')
-    {
-        j = i + 1;
+	while (i <= '9')
+	{
+		j = i + 1;
 
-        for (;j <= '9'; j++)
-        {
-            putchar(i);
-            putchar(j);
-
-            if (i == '8' && j == '9')
-            {
-                putchar('\n');
-            }
-            else
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        i++;
-    }
-    return (0);
+		for (j = i + 1 ; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == '8' && j == '9')
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+		}
+	return (0);
 }
