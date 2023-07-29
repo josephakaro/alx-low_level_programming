30ecd6c6cbb2eeaabab83e06d7314c0af882af81
josephmarko <josephmmakaro@gmail.com>
2023-07-29 10:43:36 -0700

    Completed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,15 +21,15 @@ int main(void)
 		putchar(i);
 		putchar(j);
 		putchar(k);
-		
-        if ((i == 55) && (j == 56) && (k == 57))
+
+		if (i == 55 && j == 56 && k == 57)
 		{
 			putchar('\n');
 			i++;
 			j++;
 			k++;
-        }
-        else
+		}
+		else
 		{
 			putchar(44);
 			putchar(32);
