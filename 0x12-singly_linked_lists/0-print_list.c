bd73b8944c70963ed54b9887ffae40c72442ed46
josephmarko <josephmmakaro@gmail.com>
2023-07-30 07:53:45 -0700

    Completed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,18 @@
 size_t print_list(const list_t *h)
 {
 	size_t nod;
+
 	nod = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
+
 		nod++;
 	}
 
