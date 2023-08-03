6d0120dabf14df5c9bf7ec16a19758280cf9df63
josephmarko <josephmmakaro@gmail.com>
2023-08-03 16:27:42 -0700

    Completed ✨

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,8 +13,8 @@ void print_binary(unsigned long int n)
 	if (n >> 0)
 	{
 		if (n >> 1)
-		    print_binary(n >> 1);
-		    _putchar((n & 1) + '0');
+			print_binary(n >> 1);
+			_putchar((n & 1) + '0');
 	}
 	else
 	{
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip to get from one number to another.
+ * flip_bits - returns the number of bits to flip.
  *
  * @n: first bit.
  * @m: second bit.
