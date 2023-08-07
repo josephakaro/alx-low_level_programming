38743bdd614363b86246662f217d385b81eddb90
josephmarko <josephmmakaro@gmail.com>
2023-08-07 16:27:16 -0700

    Bug Fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,13 +14,13 @@ void checkfile(int file_from, int file_to, char *argv[])
 	{
 		if (file_from == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: file %s is not readable\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 
 		if (file_to == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: file %s is not writable", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
